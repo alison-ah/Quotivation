a927d06ad6cdf30321eec2cdbdd3c29c7c4c418a
Alison <alison@andersonholland.com>
2024-11-21 09:28:12 -0600

    Filtered quotes by category

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Quotes from "./components/Quotes";
+import Quotes from "./components/Quotes/Quotes";
 import { Loader } from "react-feather";
 import "./App.css";
 
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [category, setCategory] = useState("All");
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+
+  const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
 
   const fetchQuotes = async () => {
     try {
@@ -27,10 +30,27 @@ function App() {
     fetchQuotes();
   }, []);
 
+  const filteredQuotes = category !== "All" ? quotes.filter((quote) => quote.categories.includes(category)) : quotes;
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
   return (
     <div className='App'>
       <Header />
-      <main>{loading ? <Loader /> : <Quotes quotes={quotes} />}</main>
+      <main>
+        {loading ? (
+          <Loader /> 
+          ) : ( 
+            <Quotes 
+              filteredQuotes={filteredQuotes} 
+              category={category} 
+              categories={categories}
+              handleCategoryChange={handleCategoryChange} 
+            />
+          )}
+      </main>
       <Footer />
     </div>
   );
