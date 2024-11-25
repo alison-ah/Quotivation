244a4d339d00252da6bad7d665225fe6a182bf10
Alison <alison@andersonholland.com>
2024-11-25 11:31:48 -0600

    Corrected formhtml casing

--- src/components/quotes/CategoryForm.js
+++ src/components/quotes/CategoryForm.js
@@ -11,7 +11,7 @@ function CategoryForm({ category, categories, handleCategoryChange }) {
             }}
         >
             <Filter />
-            <label htmlForm='category'>Filter Quotes:</label>
+            <label htmlform='category'>Filter Quotes:</label>
             <select id='category' name='category' value={category} onChange={handleCategoryChange}>
                 {categories.map((category) => (
                     <option key={category} value={category}>
